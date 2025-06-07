7c8a7e08aa11597af6a86fb77165caef19e858d7
Sergio <oliv.serg@gmail.com>
2025-06-07 22:32:37 +0200

    Update insert_data.py

--- insert_data.py
+++ insert_data.py
@@ -14,9 +14,6 @@ class Sale(BaseModel):
     price: float
     country: str
 
-# Connexion PostgreSQL
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-
 @router.post("/insert")
 def insert_sale(sale: Sale):
     if not DATABASE_URL:
